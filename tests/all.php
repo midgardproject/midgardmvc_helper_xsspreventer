2968032c9dd1e81592a2145951215e91b1e72343
Ferenc Szekely <ferenc.szekely@gmail.com>
2009-11-30 12:27:50 +0000

    Adding a script to replace strings in source codes. Major directory renamimgs as well, fixes #1339

---
+++ tests/all.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * @package midgardmvc_helper_xsspreventer
+ * @author The Midgard Project, http://www.midgard-project.org
+ * @copyright The Midgard Project, http://www.midgard-project.org
+ * @license http://www.gnu.org/licenses/lgpl.html GNU Lesser General Public License
+ */
+
+if (!defined('PHPUnit_MAIN_METHOD')) {
+    define('PHPUnit_MAIN_METHOD', 'midgardmvc_helper_xsspreventer_tests_all::main');
+}
+
+require_once('PHPUnit/Framework.php');
+require_once('PHPUnit/TextUI/TestRunner.php');
+
+require_once('tests/helpers.php');
+
+/**
+ * Run all XSS tests
+ *
+ * @package midgardmvc_helper_xsspreventer
+ */
+class midgardmvc_helper_xsspreventer_tests_all
+{   
+    public static function main()
+    {
+        PHPUnit_TextUI_TestRunner::run(self::suite());
+    }
+ 
+    public static function suite()
+    {
+        $suite = new PHPUnit_Framework_TestSuite('MidCOM midgardmvc_helper_xsspreventer');
+        
+        $tests = midgardmvc_core_tests_helpers::get_tests(__FILE__, __CLASS__);
+        foreach ($tests as $test)
+        {
+            $suite->addTestSuite($test);
+        }
+ 
+        return $suite;
+    }
+}
+ 
+if (PHPUnit_MAIN_METHOD == 'midgardmvc_helper_xsspreventer_tests_all::main') 
+{
+    midgardmvc_helper_xsspreventer_tests_all::main();
+}
+?>
