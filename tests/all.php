aa2dd820bb980b631ceb3499bbf89fc64b82dd56
Henri Bergius <henri.bergius@iki.fi>
2010-10-29 22:11:40 +0300

    Update to new MVC API

--- tests/all.php
+++ tests/all.php
@@ -6,30 +6,21 @@
  * @license http://www.gnu.org/licenses/lgpl.html GNU Lesser General Public License
  */
 
-if (!defined('PHPUnit_MAIN_METHOD')) {
-    define('PHPUnit_MAIN_METHOD', 'midgardmvc_helper_xsspreventer_tests_all::main');
-}
-
-require_once('PHPUnit/Framework.php');
-require_once('PHPUnit/TextUI/TestRunner.php');
-
-require_once(dirname(__FILE__) . '/../../midgardmvc_core/tests/helpers.php');
-
 /**
  * Run all XSS tests
  *
  * @package midgardmvc_helper_xsspreventer
  */
 class midgardmvc_helper_xsspreventer_tests_all
-{   
+{
     public static function main()
     {
         PHPUnit_TextUI_TestRunner::run(self::suite());
     }
- 
+
     public static function suite()
     {
-        $suite = new PHPUnit_Framework_TestSuite('Midgard MVC midgardmvc_helper_xsspreventer');
+        $suite = new PHPUnit_Framework_TestSuite("Midgard MVC XSS preventer" . __CLASS__);
         
         $tests = midgardmvc_core_tests_helpers::get_tests(__FILE__, __CLASS__);
         foreach ($tests as $test)
@@ -39,10 +30,5 @@ class midgardmvc_helper_xsspreventer_tests_all
  
         return $suite;
     }
-}
- 
-if (PHPUnit_MAIN_METHOD == 'midgardmvc_helper_xsspreventer_tests_all::main') 
-{
-    midgardmvc_helper_xsspreventer_tests_all::main();
 }
 ?>
